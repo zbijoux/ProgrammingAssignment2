6d1f80afa62ff94a7f6a93937dd2b7897e1207cb
zbijoux <zbijoux@yahoo.com>
2015-08-18 19:59:56 -0400

    ProgrammingAssignment2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,54 @@
-## Put comments here that give an overall description of what your
-## functions do
+### Caching the inverse of a Matrix###
+## We are assuming the matrix supplied is always invertible.
 
-## Write a short comment describing this function
+## makeCacheMatrix creates a Matrix,which is a list containing functions :
+## 1.setM-set the value of the Matrix
+## 2.getM-get the value of the Matrix
+## 3.setIM-set the value of the inverse of the Matrix
+## 4.getIM-get the value of the inverse of the Matrix
 
-makeCacheMatrix <- function(x = matrix()) {
+### This function makeCacheMatrix creates a special "matrix" object that can cache its inverse 
 
+makeCacheMatrix <- function(M = matrix()){
+        IM <- NULL ## an empty matrix to store the inverse of the matrix
+        
+       ## setting the value of the Matrix 
+        setM <- function(A){
+           M <- A
+          IM <<- NULL  ## IM is the inverse of the matrix M
+}
+
+     ## getting the value of the Matrix
+       getM <- function() M
+
+    ## setting the value of the inverse Matrix
+       setIM <- function(I) IM <<- I
+
+   ## getting the value of the inverse Matrix
+       getIM <- function() I 
+       list(setM=setM, getM=getM, 
+           setIM=setIM, getIM=getIM)
 }
 
 
-## Write a short comment describing this function
+### The next function calculates the inverse of the special "matrix" return by makeCacheMatrix above. 
+ 
+      
+cacheSolve <- function (M,...){
+      
+      ## Checks if the inverse has already been calculated
+        
+      IM <- M$getIM()
+      
+      ## If so gets the inverse from the cache and skip computation
+        if(!is.null(IM)){
+             message("getting cached data")
+             return(IM)
+}
+    ## otherwise, calculates the inverse and set the value of the inverse in the cache via the setIM function
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+      data <- M$get()
+        IM <- solve(data, ...)
+        M$setIM(IM)
+        IM
 }
